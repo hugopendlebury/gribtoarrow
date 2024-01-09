4bb1e09e8d068d93f544978a2566562ea0d77560
hugo pendlebury <hugo.pendlebury@gmail.com>
2024-01-09 22:08:33 +0000

    test under cmake

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,15 @@
 import os
 from pathlib import Path
 import pytest
+import sys
 
 @pytest.fixture()
 def resource():
     test_files_location = Path(os.path.realpath(__file__)).parent
-    test_path = os.path.dirname(os.path.realpath(__file__))
-    dir_path = test_path.split(os.sep)[:-1]
-    dir_path.append("build")
-    module_path = os.sep.join(dir_path)
-    #sys.path.append(module_path)
+    if os.environ.get('ARROW_CMAKE', 'N') == 'Y':
+        test_path = os.path.dirname(os.path.realpath(__file__))
+        dir_path = test_path.split(os.sep)[:-1]
+        dir_path.append("build")
+        module_path = os.sep.join(dir_path)
+        sys.path.append(module_path)
     yield test_files_location
